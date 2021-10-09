97f4182f30335715161a88f65a0e838541190d81
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2021-10-09 19:59:15 +0600

    login and userUpdate api complit

--- index.js
+++ index.js
@@ -5,88 +5,98 @@ const bodyParser=require('body-parser');
 const { MongoClient } = require('mongodb');
 const port = 4000;
 
-
-
 app.use(cors());
 app.use(bodyParser.json());
 
-
 const uri = "mongodb+srv://rifat:rifat123@cluster0.2vfwg.mongodb.net/Online-Compounder?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 
 app.get('/',(req,res)=>{
     res.send("Thank you");
 })
 
-
-
 client.connect(err => {
 
   const collection = client.db("Online-Compounder").collection("user-Details-DB");
 
-  app.post('/login',(req,res)=>{
-
+  app.post('/login',async (req,res)=>{
      //console.log(req.body)
- 
      try{
- 
-     let data = req.body
- 
-     data.slugfield=[],
-     data.token="",
-     data.registration="",
-     data.doctorChamber=[],
-     data.isAdmin=false,
-     data.isModerator=false,
-     data.isDoctor=false,
-     data.appointmentHistory=[]
- 
-     res.status(200).send({
-       msg : "succesfully new registered",
-       data
+     
+      if(req.body.email===undefined)return res.status(501).send({
+      msg : "Email required"
      })
- 
-    // console.log(req.body);
+
+      var allinfo= await collection.findOne({email:req.body.email})
+      if(allinfo===null)
+      {
+          let data = {...req.body}
+          data.slugfield=[],
+          data.token="",
+          data.registration="",
+          data.doctorChamber=[],
+          data.isAdmin=false,
+          data.isModerator=false,
+          data.isDoctor=false,
+          data.appointmentHistory=[]
+          data =await collection.insertOne(data)
+
+      }
+        data = await collection.findOne({email:req.body.email})
+     
+        res.status(200).send({
+          data 
+        })
+      }
+        catch(err)
+        {
+          res.status(500).send({
+            msg : "internal server side error"
+          })
+        }
+
+   })
  
    
-   }
-   catch(err)
-   {
-     res.status(500).send({
-       msg : "server side error"
-     })
-   }
 
-   collection.insertOne(req.body)
-   .then(result=>{
-       console.log('one Prodecut Add')
-   })
-   console.log('Data Base Conneceted')
-   })
+   app.put('/userUpdate',async (req,res)=>{
+    // req.body.email
+    // data = req.body.adminemail
 
 
+    // if(data === null || data.isAdmin==false)
 
+    try{
 
-   app.put('/userUpdate',(req,res)=>{
+      if(req.body.email===undefined)return res.status(501).send({
+        msg : "Email required"
+       })
 
-    console.log(req.body);
+      let data  = await collection.find({email:req.body.email})
 
-    collection.find({name:"Rifat"})
-    .toArray( (err,data)=>{
-      console.log(data);
-    })
-
-    
-
-    // if(req.body.name!=null)
-    // {
-      
-    // }
+      if(req.body.name!==undefined) data.name = req.body.name
+      if(req.body.pic!=undefined) data.photo=req.body.pic  
+        await collection.updateOne({email:req.body.email},
+          {
+            $set:data
+          }
+          )
+    }
+    catch(err)
+        {
+          res.status(500).send({
+            msg : "Internal server side error"
+          })
+        }
+  })
 
 
 
-   })
+
+
+
+
+   
 
 
 
