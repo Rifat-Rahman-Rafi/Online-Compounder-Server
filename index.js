85c7a94166d9a4b5a13779f01b8ab301a44ea481
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2021-10-10 00:47:59 +0600

    login and allUser/tokenUpdate

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const { MongoClient } = require('mongodb');
-const port = 4000;
+const jwt = require("jsonwebtoken");
+const { reset } = require('nodemon');
 
+const port = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -32,18 +34,35 @@ client.connect(err => {
       {
           let data = {...req.body}
           data.slugfield=[],
-          data.token="",
+          
+      data.token = jwt.sign(
+      {
+        userEmail: data.email,
+        name:data.name
+      },
+     "jhdasdkasjdkasdjaskd"
+    ),
           data.registration="",
           data.doctorChamber=[],
           data.isAdmin=false,
           data.isModerator=false,
           data.isDoctor=false,
+          data.isStatus=true,
           data.appointmentHistory=[]
           data =await collection.insertOne(data)
 
       }
+
+    //   data.token = jwt.sign(
+    //   {
+    //     userEmail: data.email,
+    //     name:data.name,
+    //     password:data.password
+    //   },
+    //  "jhdasdkasjdkasdjaskd"
+    // );
+
         data = await collection.findOne({email:req.body.email})
-     
         res.status(200).send({
           data 
         })
@@ -56,40 +75,106 @@ client.connect(err => {
         }
 
    })
- 
-   
 
-   app.put('/userUpdate',async (req,res)=>{
-    // req.body.email
-    // data = req.body.adminemail
-
-
-    // if(data === null || data.isAdmin==false)
-
+   //all user token update
+   app.put('/allUser/tokenUpdate',async(req,res)=>{
     try{
+     let adminToken=req.body.adminToken
+     let tokenString=req.body.tokenString
+     if(adminToken===undefined||tokenString===undefined)return res.status(501).send({
+      msg : "Email required"
+     })
+     let adminDetails =await collection.findOne({token:adminToken})
+     if(adminDetails==null || adminDetails.isAdmin===false)
+     return res.status(401).send({
+       msg : 'please send an admin token'
+     })
 
-      if(req.body.email===undefined)return res.status(501).send({
-        msg : "Email required"
-       })
+    //  console.log(adminDetails)
 
-      let data  = await collection.find({email:req.body.email})
-
-      if(req.body.name!==undefined) data.name = req.body.name
-      if(req.body.pic!=undefined) data.photo=req.body.pic  
-        await collection.updateOne({email:req.body.email},
-          {
-            $set:data
-          }
+    let data=await collection.find({}).toArray()
+       for(let i=0;i<data.length;i++)
+       {
+         if(data[i].token===adminToken) continue;
+         else{
+          data[i].token = jwt.sign(
+            {
+              userEmail: data[i].email,
+              name:data[i].name
+            },
+           tokenString
           )
+
+         }
+         await collection.updateOne({_id : data[i]._id},{$set: {"token" : data[i].token}})
+       }
+       res.status(200).send({})
     }
     catch(err)
         {
           res.status(500).send({
-            msg : "Internal server side error"
+            msg : "internal server side error"
           })
         }
-  })
+   })
+ 
+   
 
+  //  app.put('/userUpdate',async (req,res)=>{
+  //   // req.body.email
+  //   // data = req.body.adminemail
+  //   // if(data === null || data.isAdmin==false)
+  //   try{
+
+  //     if(req.body.email===undefined)return res.status(501).send({
+  //       msg : "Email required"
+  //      })
+
+  //     let data  = await collection.find({email:req.body.email})
+
+  //     if(req.body.name!==undefined) data.name = req.body.name
+  //     if(req.body.pic!=undefined) data.photo=req.body.pic  
+  //       await collection.updateOne({email:req.body.email},
+  //         {
+  //           $set:data    
+  //         }
+  //         )
+  //         res.status(200).send({
+  //           msg : "Successfully Updated Data"
+  //         })
+  //   }
+  //   catch(err)
+  //       {
+  //         res.status(500).send({
+  //           msg : "Internal server side error"
+  //         })
+  //       }
+  // })
+
+
+
+
+    
+  // app.put('/userUpdateByAdmin',(req,res)=>{
+   
+  //   try{
+         
+  //     if(req.body.email===undefined || req.body.adminEmail==undefined)return res.status(501).send({
+  //       msg : "Email required"
+  //      })
+
+      
+
+  //   }
+  //   catch(err)
+  //   {
+  //     res.status(500).send({
+  //       msg : "Internal server side error"
+  //     })
+  //   }
+    
+
+  // })
 
 
 
